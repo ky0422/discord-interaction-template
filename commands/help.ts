e0d3702d01900c9eeb528566848272628e8938f7
tsukiroku <tsukiroku@yahoo.com>
2022-05-17 18:19:15 +0900

    Create help.ts

---
+++ commands/help.ts
@@ -0,0 +1,14 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { MessageComponentInteraction } from 'discord.js';
+
+export default {
+    process: (interaction: MessageComponentInteraction) => {
+        interaction.reply({
+            content: `help`,
+            ephemeral: true,
+        });
+    },
+    command: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription('Help Command owo'),
+};
