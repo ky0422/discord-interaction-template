2108f84e653e28ed7197986b9d0c7bb9695ad6d7
tsukiroku <tsukiroku@yahoo.com>
2022-05-17 20:43:14 +0900

    Update commandRegister.ts

--- src/commandRegister.ts
+++ src/commandRegister.ts
@@ -1,13 +1,15 @@
 import { ApplicationCommandDataResolvable } from 'discord.js';
 import fs from 'fs';
+import { logger } from '..';
 
 export default async () => {
     const commands: Array<ApplicationCommandDataResolvable> = Array();
-    await fs.readdirSync('./dist/commands').forEach(async (file) => {
+    fs.readdirSync('./dist/commands').forEach(async (file) => {
         await import(`../commands/${file}`).then(async (command) => {
             const _command = command.default.command;
             if (!_command) return;
-            await commands.push(_command.toJSON());
+            logger.info(`Command ${_command.name} (${file}) loaded.`);
+            commands.push(_command.toJSON());
         });
     });
     return commands;
