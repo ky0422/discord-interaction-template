4e6ffdd7c59e2a5b870ab4ea5a93193a8584ffdf
tsukiroku <tsukiroku@yahoo.com>
2022-05-25 17:39:47 +0900

    Create config.ts

---
+++ config.ts
@@ -0,0 +1,15 @@
+import { Intents, IntentsString } from 'discord.js';
+
+interface Config {
+    token: string;
+    client_id: string;
+    dev_guild: string;
+    intents: Array<IntentsString | number>;
+}
+
+export default {
+    token: process.env.TOKEN,
+    client_id: process.env.CLIENT_ID,
+    dev_guild: process.env.DEV_GUILD,
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+} as Config;
