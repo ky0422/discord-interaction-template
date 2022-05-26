db975dc9b6a33f0c80a788ff76140434c8be6cf9
tsukiroku <tsukiroku@yahoo.com>
2022-05-26 19:09:07 +0900

    Update config.ts

--- config.ts
+++ config.ts
@@ -3,13 +3,13 @@ import { Intents, IntentsString } from 'discord.js';
 interface Config {
     token: string;
     client_id: string;
-    dev_guild: string;
+    dev_guild?: string;
     intents: Array<IntentsString | number>;
 }
 
 export default {
     token: process.env.TOKEN,
     client_id: process.env.CLIENT_ID,
-    dev_guild: process.env.DEV_GUILD,
+    // dev_guild: process.env.DEV_GUILD,
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 } as Config;
