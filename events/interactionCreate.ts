fe3b33447d35c1c22a9635976f6a9c5c4dd67a40
tsukiroku <tsukiroku@yahoo.com>
2022-05-31 20:54:00 +0900

    Update interactionCreate.ts

--- events/interactionCreate.ts
+++ events/interactionCreate.ts
@@ -3,12 +3,10 @@ import { Interaction } from 'discord.js';
 export default async (interaction: Interaction) => {
     if (!interaction.isCommand()) return;
     import(`../commands/${interaction.commandName.replace('dev_', '')}`)
-        .then(async (cmd) => {
-            await cmd.default.process(interaction);
-        })
-        .catch(async () => {
+        .then(async (cmd) => await cmd.default.process(interaction))
+        .catch(async () =>
             import('../commands/default').then(async (cmd) => {
                 cmd.default.process(interaction as any);
-            });
-        });
+            })
+        );
 };
