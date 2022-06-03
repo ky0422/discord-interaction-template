fceb8da174179db3f343ae3cb5f1aab02b3ad197
tsukiroku <tsukiroku@yahoo.com>
2022-06-03 16:42:33 +0900

    Update interactionCreate.ts

--- events/interactionCreate.ts
+++ events/interactionCreate.ts
@@ -1,7 +1,8 @@
 import { Interaction } from 'discord.js';
 
+// If you don't have knowledge of this, don't modify the code.
 export default async (interaction: Interaction) => {
-    if (!interaction.isCommand()) return;
+    if (!interaction.isCommand() && !interaction.isContextMenu()) return;
     import(`../commands/${interaction.commandName.replace('dev_', '')}`)
         .then(async (cmd) => await cmd.default.process(interaction))
         .catch(async () =>
