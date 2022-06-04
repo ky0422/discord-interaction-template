b819dfedafab47548e8a28d9a5cec8aec85662bd
tsukiroku <tsukiroku@yahoo.com>
2022-06-04 21:46:43 +0900

    Update config.ts

--- config.ts
+++ config.ts
@@ -4,6 +4,7 @@ export interface Config {
     token: string;
     client_id: string;
     dev_guild?: string;
+    owner_id: string;
     intents: Array<IntentsString | number>;
 }
 
@@ -11,5 +12,6 @@ export default {
     token: process.env.TOKEN,
     client_id: process.env.CLIENT_ID,
     dev_guild: process.env.DEV_GUILD,
+    owner_id: process.env.OWNER_ID,
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 } as Config;
