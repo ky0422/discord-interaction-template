a8fe9a202ded8d203ad5bb78e701d7a75f13f71e
tsukiroku <tsukiroku@yahoo.com>
2022-06-04 21:46:40 +0900

    Create eval.ts

---
+++ commands/eval.ts
@@ -0,0 +1,40 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import {
+    CommandInteractionOptionResolver,
+    MessageComponentInteraction,
+} from 'discord.js';
+import config from '../config';
+
+export default {
+    process: (interaction: MessageComponentInteraction) => {
+        if (interaction.user.id !== config.owner_id) {
+            interaction.reply({
+                content: '**You are not the owner of this bot.**',
+                ephemeral: true,
+            });
+            return;
+        }
+
+        try {
+            const result: string = eval(
+                (interaction as any).options.getString('code')
+            );
+            interaction.reply({
+                content: `\`\`\`ts\n${result.replace(
+                    /[a-zA-Z\--_]{24}\.[a-zA-Z\--_]{6}\.[a-zA-Z\--_]{38}/g,
+                    'TOKEN'
+                )}\n\`\`\``,
+            });
+        } catch (e) {
+            interaction.reply({
+                content: `\`\`\`ts\n${e}\n\`\`\``,
+            });
+        }
+    },
+    command: new SlashCommandBuilder()
+        .setName('eval')
+        .setDescription('Dangerous command. be careful when using it.')
+        .addStringOption((option) =>
+            option.setName('code').setDescription('eval').setRequired(true)
+        ),
+};
