96f52c032456e2448b463ea84975d0adaa8dcaf7
tsukiroku <tsukiroku@yahoo.com>
2022-06-07 17:29:21 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -5,13 +5,14 @@ import BotClient from './src/bot';
 
 if (!config.token || !(typeof config.token === 'string'))
     throw new Error(
-        `Token is an invalid value.\nSee \`https://github.com/tsukiroku/discord-interaction-template/issues/1#issuecomment-1145866930\` for more info.\n`
+        `Token is an invalid value.\nSee \`https://github.com/tsukiroku/discord-interaction-template/issues/1#issuecomment-1148360826\` for more info.\n`
     );
 
-const client = new BotClient();
+const client = new BotClient({
+    handleInteraction: interactionCreate,
+});
 
 client.on('ready', ready);
-client.on('interactionCreate', interactionCreate);
 
 client.login(config.token);
 
