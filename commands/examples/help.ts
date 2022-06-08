d74e98ab68874a98838eb37e1e830b0437f5b057
tsukiroku <tsukiroku@yahoo.com>
2022-06-08 22:48:44 +0900

    Update help.ts

--- commands/examples/help.ts
+++ commands/examples/help.ts
@@ -1,14 +1,47 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { MessageComponentInteraction } from 'discord.js';
+import { CommandInteraction, MessageComponentInteraction } from 'discord.js';
 
 export default {
     process: (interaction: MessageComponentInteraction) => {
-        interaction.reply({
-            content: `help`,
-            ephemeral: true,
-        });
+        const subCommand = (
+            interaction as unknown as CommandInteraction
+        ).options.getSubcommand();
+
+        if (subCommand === 'all') {
+            interaction.reply({
+                content: `
+> **\`(dev_)[command]\`**
+> 
+> \`/button\`
+> \`/select_menu\`
+> \`/eval [code]\`
+                `,
+                ephemeral: true,
+            });
+        } else if (subCommand === 'owner') {
+            interaction.reply({
+                content: `
+> **\`(dev_)[command]\`**
+> 
+> \`/eval [code]\`
+                `,
+                ephemeral: true,
+            });
+        } else {
+            interaction.reply('`/help [all | owner]`');
+        }
     },
     command: new SlashCommandBuilder()
         .setName('help')
-        .setDescription('Help command'),
+        .setDescription('Help command')
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName('all')
+                .setDescription('Show help for all commands')
+        )
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName('owner')
+                .setDescription('Show help for owner commands')
+        ),
 };
