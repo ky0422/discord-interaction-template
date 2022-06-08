b6f1b8d6f369c229a1a193b899d326e8a17b426a
tsukiroku <tsukiroku@yahoo.com>
2022-06-08 22:19:44 +0900

    Update types.ts

--- utils/types.ts
+++ utils/types.ts
@@ -1,5 +1,43 @@
+import {
+    Awaitable,
+    Client,
+    ClientEvents,
+    ClientOptions,
+    Interaction,
+} from 'discord.js';
+import { Config } from '../config';
+import { Logger } from './logger';
+
 type R_Optional<T> = {
     [K in keyof T]-?: T[K];
 };
 
-export { R_Optional };
+interface IBotOptions {
+    readonly logger?: Logger<string>;
+    readonly config?: Config;
+    readonly path?: PathOptions;
+    readonly clientOptions?: ClientOptions;
+    readonly handleInteraction?: (
+        interaction: Interaction,
+        path: string,
+        default_path: string
+    ) => any;
+}
+
+interface IBot extends IBotOptions {
+    _client: Client;
+    logger: Logger<string>;
+    config: Config;
+    login: (token: string) => Promise<any>;
+    on: <K extends keyof ClientEvents>(
+        event: K,
+        callback: (...args: ClientEvents[K]) => Awaitable<void>
+    ) => any;
+}
+
+interface PathOptions {
+    readonly path?: string;
+    readonly defaultPath?: string;
+}
+
+export { R_Optional, IBotOptions, IBot, PathOptions };
