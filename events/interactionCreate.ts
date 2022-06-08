b6327899c77adf0323711780150b8097cf7c2dcb
tsukiroku <tsukiroku@yahoo.com>
2022-06-08 22:19:36 +0900

    Update interactionCreate.ts

--- events/interactionCreate.ts
+++ events/interactionCreate.ts
@@ -3,12 +3,16 @@
 import { Interaction } from 'discord.js';
 import client from '..';
 
-export default async (interaction: Interaction, path: string) => {
+export default async (
+    interaction: Interaction,
+    path: string,
+    default_path: string
+) => {
     if (!interaction.isCommand() && !interaction.isContextMenu()) return;
     import(`../${path}/${interaction.commandName.replace('dev_', '')}`)
         .then(async (command) => await command.default.process(interaction))
         .catch(async (e) =>
-            import(`../${path}/default`).then(async (command) => {
+            import(`../${path}/${default_path}`).then(async (command) => {
                 command.default.process(interaction as any);
                 client.logger.error(e);
             })
