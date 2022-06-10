8c613472af626a435e5fc2e6745da8cc63a0caab
tsukiroku <tsukiroku@yahoo.com>
2022-06-10 17:03:48 +0900

    Update context_menu.ts

--- commands/examples/context_menu.ts
+++ commands/examples/context_menu.ts
@@ -1,8 +1,8 @@
 import { ContextMenuCommandBuilder } from '@discordjs/builders';
-import { ContextMenuInteraction } from 'discord.js';
+import { IContextMenu } from '../../utils/types';
 
 export default {
-    process: (interaction: ContextMenuInteraction) => {
+    process: (interaction: IContextMenu) => {
         interaction.reply({
             content: `${interaction.options.data[0].message?.content}`,
             ephemeral: true,
